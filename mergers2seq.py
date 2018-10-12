a3384215bc0aa9efc0ca66be24af1c7ac3399d7d
TorHou <torsten.houwaart@med.uni-duesseldorf.de>
2018-10-12 20:15:09 +0200

    Seems to terminate. good start

--- mergers2seq.py
+++ mergers2seq.py
@@ -14,10 +14,11 @@ parser.add_argument("outfile", help="fasta-outputfile for scaffolds")
 args = parser.parse_args()
 
 """ thaz how it look like
-incorporation	cluster_6	d625a932-ce76-4d38-a486-c708414e4c37	193950	cluster_7
-incorporation	cluster_7	ebb2adf5-7217-4f60-aca3-1f3e6205ee41	211867	cluster_8
-incorporation	cluster_8	02fa0a3d-0721-4017-948f-3eeff063c6c9	203035	cluster_9
-extension	cluster_9	ab0cb3e0-e72a-4ac9-8d3d-f86cc2867a7e	239134	cluster_10
+incorporation	cluster_6	280489	d625a932-ce76-4d38-a486-c708414e4c37	19987	193950	cluster_7
+incorporation	cluster_7	280489	ebb2adf5-7217-4f60-aca3-1f3e6205ee41	3214	211867	cluster_8
+incorporation	cluster_8	280489	02fa0a3d-0721-4017-948f-3eeff063c6c9	6013	203035	cluster_9
+extension	cluster_9	280489	ab0cb3e0-e72a-4ac9-8d3d-f86cc2867a7e	104531	239134	cluster_10
+
 """
 
 
@@ -25,17 +26,22 @@ dot = nx.DiGraph()
 
 with open(args.input) as f:
     for line in f:
-        [modestring, node1, node2, distance, newnode] = line.split()
+        [modestring, node1, l1, node2, l2, distance, newnode] = line.split()
         if not node1.startswith("cluster"):
-            dot.add_node(node1)
+            dot.add_node(node1, length = l1)
         if not node2.startswith("cluster"):
-            dot.add_node(node2)
+            dot.add_node(node2, length = l2)
         dot.add_node(newnode, mode = modestring)
         dot.add_edge(node1, newnode)
         dot.add_edge(newnode, node1, link = "left")
         dot.add_edge(node2,newnode)
         if modestring == "extension":
+            new_length = distance + l2
             dot.add_edge(newnode, node2, link = "right")
+        else:
+            new_length = l1
+        dot.nodes[newnode]["length"] = new_length
+        
 
 #print(dot.nodes["cluster_1"])
 #print(dot.nodes["653b2764-e21e-458a-99e4-702f7eac052a"])
@@ -63,6 +69,7 @@ for parent in parents:
 def get_start_node(g, end_node):
     n = end_node
     while True:
+        print("getting start node from: " + n)
         for node2, edge in g[n].items():
             if "link" in edge and edge["link"] == "left":
                 n = node2
@@ -81,8 +88,6 @@ for node in dot.nodes:
     else:
         final_nodes.append(node)
 
-print(final_nodes[0])
-print(get_start_node(dot, final_nodes[0]))
     
 def suffix(path,offset):
     suffix = []
@@ -94,14 +99,66 @@ def suffix(path,offset):
             suffix.append(((part[0]-offset, part[1]-offset), name))
     return suffix
 
-def find_path(g, n1, n2):
-    path = [((1,g.nodes[n1]["length"]),n1)]
+# move to next extension node
+# up until n2 !!
+def move_to_extension(g, n1, n2):
+    print("Move to Extension " + n1 + " and " + n2)
+    current_node = go_down(g, n1)
+    #print("current_node: " + current_node)
+
     while True:
-        current_node = move_to_extension(n1)
+        #print(current_node + " is  current_node" )
+        if current_node == n2:
+            return current_node
+        if "mode" in g.nodes[current_node] and g.nodes[current_node]["mode"] == "extension":
+            print("extension found at node: " + current_node)
+            return current_node
+        else:
+            current_node = go_down(g, current_node)
+            #print("current_node: " + current_node)
+
+def go_right(g, n):
+    for n2 in g[n]:
+        if "link" in g[n][n2] and g[n][n2]["link"] == "right":
+            return n2
+    else:
+        print("Problem finding right neigbour")
+        sys.exit(0)
+
+def go_down(g, n):
+    #print("going down from: " + n)
+    for n2 in g[n]:
+        if "link" in g[n][n2]:
+            continue
+        else:
+            return n2
+    else:
+        return n2
+                
+                
+def find_path(g, n1, n2):
+    print("Finding path between " + n1 + " and " + n2)
+    path = [((1,g.nodes[n1]["length"]),n1, 0)]
+    if n1 == n2:
+        return path
+    current_node = n1
+    while True:
+        current_node = move_to_extension(g, current_node, n2)
+        if "mode" in g.nodes[current_node] and g.nodes[current_node]["mode"] == "extension":
+            rn = go_right(g, current_node)
+            path.append(find_path(g, get_start_node(g, rn) ,rn))
         if current_node == n2:
             return path
-        else:
-            path = n1
+
+#print(dot.nodes)
+print(dot.nodes["cluster_1"]["mode"])
+print(final_nodes[0])
+sn = get_start_node(dot, final_nodes[0])
+print(dot["cluster_277"]["cluster_276"])
+print(dot.nodes["cluster_277"])
+print(dot.nodes["cluster_276"])
+#sys.exit(0)
+print(find_path(dot, sn, final_nodes[0]))
 
 # TODO
 '''
@@ -112,7 +169,6 @@ find_path n1 n2:
     return n1
 '''
 
-def 
 
 
 
