d04261693caa8b9d319aaa7fe250ca2fdfc8fdb0
Torsten Houwaart <houwaart@informatik.uni-freiburg.de>
2018-10-25 14:46:28 +0200

    Added aligning of reads

--- erate2SVG.py
+++ erate2SVG.py
@@ -2,6 +2,8 @@ from argparse import ArgumentParser
 import svgwrite
 from operator import itemgetter
 import sys
+from itertools import combinations
+from random import sample
 
 
 
@@ -10,7 +12,7 @@ parser.add_argument("efile", help="Error rate file")
 parser.add_argument("--whitelist", help="Only plot long reads with ids found in this whitelist file.")
 parser.add_argument("--blacklist", help="Do not plot long reads in this file.")
 parser.add_argument("--alignreads", action="store_true", help="Reads will be turned around if necessary and aligned according to their distances.")
-parser.add_argument("cellline", help="Name of the cellline")
+parser.add_argument("linename", help="Name of the cellline")
 parser.add_argument("output", help="SVG output file")
 
 args = parser.parse_args()
@@ -60,21 +62,21 @@ intreads_contigset = set()
 for rid, read in lreads.items():
     counter = 0
     for item in read["maps"]:
-        if item["contig"].endswith(args.cellline):
+        if item["contig"].endswith(args.linename):
             counter += 1
     if counter >= 2:
         greadst[rid] = read
         for item in read["maps"]:
-            if item["contig"].endswith(args.cellline):
+            if item["contig"].endswith(args.linename):
                 greads_contigset.add(item["contig"])
     if counter == 1:
         intreads[rid] = read
         for item in read["maps"]:
-            if item["contig"].endswith(args.cellline):
+            if item["contig"].endswith(args.linename):
                 intreads_contigset.add(item["contig"])
             
 intersection = greads_contigset.intersection(intreads_contigset)
-sintersection = sorted(intersection, key = lambda x: int(x.rstrip(args.cellline)))
+sintersection = sorted(intersection, key = lambda x: int(x.rstrip(args.linename)))
 #print(sintersection)
 
 
@@ -83,6 +85,40 @@ for rid in intreads:
         pass
         #print("\t".join([rid, str(intreads[rid])]))
         
+# turn reads around
+
+# turn reads around if necessary
+for rid, lr in greadst.items():
+    bw = 0
+    fw = 0
+    for mapping in lr["maps"]:
+        if mapping["contig"].endswith(args.linename): 
+            if mapping["strand"] == 1:
+                bw += 1
+            elif mapping["strand"] == 0:
+                fw += 1
+            else:
+                raise ValueError("strand: " + str(mapping["strand"]))
+    if bw > fw:
+        for mapping in lr["maps"]:
+            if mapping["contig"].endswith(args.linename): 
+                mapping["strand"] = 1 if mapping["strand"] == 0 else 0
+                tmp = mapping["scr"]
+                mapping["scr"] = lr["length"] - mapping["ecr"]
+                mapping["ecr"] = lr["length"] - tmp
+                tmp = mapping["scc"]
+                mapping["scc"] = mapping["lenc"] - mapping["ecc"]
+                mapping["ecc"] = mapping["lenc"] - tmp
+        
+    # turn around and redefine wrong contigs
+    for mapping in lr["maps"]:
+        if mapping["contig"].endswith(args.linename): 
+            if mapping["strand"] == 1: #define a new contigname and turn it around
+                mapping["contig"] = mapping["contig"] + "rc"
+                mapping["scc"] = mapping["lenc"] - mapping["ecc"]
+                mapping["ecc"] = mapping["lenc"] - mapping["scc"]
+                mapping["strand"] = 0
+
 
 # sort contigs by left coordinate
 for rid in greadst:
@@ -91,7 +127,7 @@ for rid in greadst:
     greads[rid]["maps"]=soverlaps
 
 ogreads = greads.copy()
-print("lgnth: " + str(len(greadst)))
+print("length greads: " + str(len(greads)))
 
 
 # cluster np-reads or only keep whitelisted ones
@@ -138,6 +174,94 @@ else:
         print("cluster length: " + str(len(current_cluster)))
         creads[clusternr] = current_cluster
 
+def compare_longreads(lr1, lr2):
+    l1c = []
+    l2c = []
+    for m in lr1["maps"]:
+        cn = m["contig"]
+        if cn.endswith(args.linename):
+            l1c.append(cn)
+    for m in lr2["maps"]:
+        cn = m["contig"]
+        if cn.endswith(args.linename):
+            l2c.append(cn)
+    common_ctgs = set(l1c).intersection(set(l2c))
+    return common_ctgs
+
+
+def get_contig_info(lr,ctg):
+    for maps in lr["maps"]:    
+        if maps["contig"] == ctg:
+            return maps
+
+# calculate distances for each cluster
+def get_distances(lr1,lr2, common_ctgs):
+    dists = []
+    for ctg in common_ctgs:
+        m1 = get_contig_info(lr1,ctg)
+        m2 = get_contig_info(lr2,ctg)
+        dists.append((m1["scr"]-m1["scc"]) - (m2["scr"]-m2["scc"]))
+    return dists
+            
+#lr_dist = defaultdict(lambda:([],[]))
+lr_dists = {}
+#initialize matrix
+for lid, read in lreads.items():
+    lr_dists[lid] = {lid:0}
+
+for lrs in combinations(ogreads.keys(), 2):
+    lr1 = ogreads[lrs[0]]
+    lr2 = ogreads[lrs[1]]
+    common_ctgs = compare_longreads(lr1, lr2)
+    if len(common_ctgs) > 0:
+        dists = get_distances(lr1, lr2, common_ctgs)
+        lr_dists[lrs[0]][lrs[1]]=dists[0]
+        lr_dists[lrs[1]][lrs[0]]=-dists[0]
+
+            
+for cluster in creads:
+    #print(creads[cluster].keys())
+    # find leftmost read 
+    while True:
+        rid = sample(creads[cluster].keys(),1)[0]
+        for rid2 in creads[cluster].keys():
+            if rid2 in lr_dists[rid]:
+                if lr_dists[rid][rid2] < 0:
+                    rid = rid2
+                    break
+        break
+    srid = rid
+
+    # fill lr_dists with transitive distances
+    A = set(creads[cluster].keys())
+    B = set(lr_dists[srid].keys())
+    U = A-B
+    UC = U.copy()
+    while True:
+        for rid in U:
+            for rid2 in lr_dists[rid]:
+                if rid2 not in UC:
+                    lr_dists[srid][rid] = lr_dists[srid][rid2] + lr_dists[rid2][rid]
+                    lr_dists[rid][srid] = lr_dists[srid][rid]
+                    UC.remove(rid)
+                    break
+        U = UC.copy()
+        if not U:
+            break
+   
+    # now there is a new leftmost read
+    smval = 0
+    nsrid = srid
+    for rid2 in lr_dists[srid]:
+        if lr_dists[srid][rid2] < smval:
+            smval = lr_dists[srid][rid2]
+            nsrid = rid2
+
+    # rid should be smallest
+    creads[cluster]["smallest_id"] = srid
+    creads[cluster]["smallest_offset"] = -smval
+#sys.exit(0)
+
 
 # draw interesting reads
 ypos = 0
@@ -146,27 +270,28 @@ xpad = 200
 ypad = 10
 dwg = svgwrite.Drawing(args.output, size=(u'1700', u'4600'), profile='full')
 csize = 0
-bold = False
 for cluster in creads:
-    bold = not bold
-    for rid in creads[cluster].items():
-        ypos += 20
-        if bold:
-            dwg.add(dwg.text(rid[0], insert=(xtext, ypad+ypos+1), fill='red', style="font-size:7"))
+    for rid,read in creads[cluster].items():
+        if rid.startswith("small"):
+            continue
+        if args.alignreads:
+            xoffset = creads[cluster]["smallest_offset"] + lr_dists[creads[cluster]["smallest_id"]][rid]
         else:
-            dwg.add(dwg.text(rid[0], insert=(xtext, ypad+ypos+1), fill='black', style="font-size:7"))
-        dwg.add(dwg.line((xpad, ypad+ypos), ( xpad + ogreads[rid[0]]["length"]/100, ypad+ypos), stroke=svgwrite.rgb(0, 0, 0, '%')))
+            xoffset = 0
+        ypos += 20
+        dwg.add(dwg.text(rid, insert=(xtext, ypad+ypos+1), fill='black', style="font-size:7"))
+        dwg.add(dwg.line((xpad+ xoffset/100, ypad+ypos), ( xpad + (xoffset+ogreads[rid]["length"])/100, ypad+ypos), stroke=svgwrite.rgb(0, 0, 0, '%')))
         above = True
         col = "black"
-        for read in ogreads[rid[0]]["maps"]:
+        for read in ogreads[rid]["maps"]:
             #print(read)
             sc = read["scr"]
             ec = read["ecr"]
-            ctg = read["contig"].rstrip(args.cellline)
+            ctg = read["contig"].rstrip(args.linename)
             #ctg = read[0]
             if ctg.startswith("chr"):
                 ctg = ctg[0:8]
-            dwg.add(svgwrite.shapes.Rect((xpad+(sc/100),ypad+ypos-3), ((ec-sc)/100,6), stroke='black', stroke_width=1, fill = 'white'))
+            dwg.add(svgwrite.shapes.Rect((xpad+((xoffset+sc)/100),ypad+ypos-3), ((ec-sc)/100,6), stroke='black', stroke_width=1, fill = 'white'))
             if above:
                 yt = ypad+ypos-4
                 if col == "blue":
@@ -176,12 +301,12 @@ for cluster in creads:
             else:
                 yt = ypad+ypos+7
             above = not above
-            dwg.add(dwg.text(ctg, insert=(xpad+sc/100,yt),fill=col, style="font-size:4"))
+            dwg.add(dwg.text(ctg, insert=(xpad+(xoffset+sc)/100,yt),fill=col, style="font-size:4"))
             if read["strand"] == 0:
                 direction = ">"
             elif read["strand"] == 1:
                 direction = "<"
-            dwg.add(dwg.text(direction, insert=(xpad+sc/100,ypad+ypos+2),style="font-size:6"))
+            dwg.add(dwg.text(direction, insert=(xpad+(xoffset+sc)/100,ypad+ypos+2),style="font-size:6"))
     dwg.add(dwg.line((10, ypad+ypos+10), ( 1000, ypad+ypos+10), stroke=svgwrite.rgb(0, 0, 0, '%')))
 
 
